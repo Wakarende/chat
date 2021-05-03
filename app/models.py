a2dee55f142f275da8b1925eef71d2de579a0254
Joy Kirii <joykirii@gmail.com>
2021-05-03 19:49:55 +0300

    add register section html

--- app/models.py
+++ app/models.py
@@ -3,6 +3,12 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
 
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(int(user_id))
+
+
+
 class User(UserMixin,db.Model):
   __tablename__ = 'users'
   id = db.Column(db.Integer,primary_key = True)
