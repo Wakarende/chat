0730e18e848cc9f1285ae43b4cfdf2e32cb48a5d
Joy Kirii <joykirii@gmail.com>
2021-05-03 19:33:13 +0300

    created registration flask form

--- app/models.py
+++ app/models.py
@@ -1,11 +1,14 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
 
-class User(db.Model):
+class User(UserMixin,db.Model):
   __tablename__ = 'users'
   id = db.Column(db.Integer,primary_key = True)
   username = db.Column(db.String(255))
   pass_secure = db.Column(db.String(255))
+  email = db.Column(db.String(255),unique = True,index = True)
 
   @property
   def password(self):
