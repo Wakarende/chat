29f830fc9cf38b0b8abb7093b947066ef66b79fb
Joy Kirii <joykirii@gmail.com>
2021-05-05 16:20:07 +0300

    create requests

--- config.py
+++ config.py
@@ -8,6 +8,15 @@ class Config:
   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://joykirii:kirii@localhost/chat'
   UPLOADED_PHOTOS_DEST ='app/static/photos'
 
+  #Spotify API
+  CLIENT_Id='3acfb5c708b1421888f1d5d74388e27f'
+  CLIENT_SECRET='8fa76f281cf540af838095c203ce2c53'
+  ACCESS_TOKEN_URL='https://accounts.spotify.com/api/token'
+  AUTH_URL='http://accounts.spotify.com/authorize'
+  API_VERSION='v1'
+  API_URL= 'https://api.spotify.com'
+  AUTH_METHOD= 'AUTHORIZATION_CODE'
+
 class ProdConfig(Config):
 
   pass
