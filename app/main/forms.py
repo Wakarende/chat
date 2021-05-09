43f1e7d4300c4aaceb7aa6eb70afdd7df6344ae0
Joy Kirii <joykirii@gmail.com>
2021-05-09 10:49:40 +0300

    update forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -15,14 +15,24 @@ class PlaylistForm(FlaskForm):
   name = StringField("Playlist Name", validators=[InputRequired()])
   # description = StringField('Playlist Description', validators=[InputRequired()])
   submit = SubmitField('Submit')
-  
+ 
+
 class SongForm(FlaskForm):
   """Form for adding songs."""
-  title = StringField("Song Title", validators=[InputRequired()] )
-  artist = StringField("Artist Name", validators=[InputRequired()] )
-  submit = SubmitField('Submit')
+  # Add the necessary code to use this form
+  title = StringField("Song Title", validators=[InputRequired()])
+  artist = StringField("Artist Name", validators=[InputRequired()])
 
+
+# DO NOT MODIFY THIS FORM - EVERYTHING YOU NEED IS HERE
 class NewSongForPlaylistForm(FlaskForm):
   """Form for adding a song to playlist."""
-  song = SelectField('Song To Add', coerce=int)
-  submit = SubmitField('Submit')
+
+
+class SearchSongsForm(FlaskForm):
+  """Form for searching music"""
+  track = StringField("Search for song or word on a song", validators=[InputRequired()])
+
+
+class DeleteForm(FlaskForm):
+  """Delete form -- this form is intentionally blank."""
